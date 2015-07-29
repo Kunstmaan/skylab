eade2e1f552af78874ca78d0066374abf3af8b15
Kris Pypen <kris.pypen@gmail.com>
2015-07-29 11:50:08 +0200

    fix compiler should also include .pem files from vendors

--- src/Kunstmaan/Skylab/Compiler.php
+++ src/Kunstmaan/Skylab/Compiler.php
@@ -59,6 +59,7 @@ class Compiler
         $finder->files()
             ->ignoreVCS(true)
             ->name('*.php')
+            ->name('*.pem')
             ->exclude('Tests')
             ->in(__DIR__ . '/../../../vendor/');
 
