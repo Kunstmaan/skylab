fb8ce11ce5339f33bbe5720f3f94fc312cf8b8f5
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2015-08-10 10:23:10 +0200

    Don't fail on unmigrated projects

--- src/Kunstmaan/Skylab/Skeleton/WebserverSkeleton.php
+++ src/Kunstmaan/Skylab/Skeleton/WebserverSkeleton.php
@@ -322,7 +322,7 @@ class WebserverSkeleton extends AbstractSkeleton
     function checkObviousErrors(\ArrayObject $project, SplFileInfo $config, $content){
         // project was not migrated because the 19php.conf file does not contain "proxy:unix:/var/run/php5-fpm"
         if (strpos($config->getFilename(), "19php") !== FALSE && strpos($content, "proxy:unix:/var/run/php5-fpm") === FALSE){
-            throw new SkylabException("The ".$project["name"]." project was not migrated yet, this will NOT work");
+            $this->dialogProvider->logWarning("The ".$project["name"]." project was not migrated yet, this will NOT work");
         }
     }
 
