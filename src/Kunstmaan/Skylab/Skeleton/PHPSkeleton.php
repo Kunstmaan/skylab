237efdc4ad29979f39685c8e66076d55d405b5ce
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2016-04-10 21:07:41 +0200

    Enable PHP7 support

--- src/Kunstmaan/Skylab/Skeleton/PHPSkeleton.php
+++ src/Kunstmaan/Skylab/Skeleton/PHPSkeleton.php
@@ -28,16 +28,16 @@ class PHPSkeleton extends AbstractSkeleton
      */
     public function create(\ArrayObject $project)
     {
-        $this->fileSystemProvider->createDirectory($project, 'php5-fpm');
+        $this->fileSystemProvider->createDirectory($project, 'php-fpm');
         {
             $permissionDefinition = new PermissionDefinition();
-            $permissionDefinition->setPath("/php5-fpm");
+            $permissionDefinition->setPath("/php-fpm");
             $permissionDefinition->setOwnership("-R @project.user@.@project.group@");
             $permissionDefinition->addAcl("-R -m user::rwX");
             $permissionDefinition->addAcl("-R -m group::r-X");
             $permissionDefinition->addAcl("-R -m other::---");
             $permissionDefinition->addAcl("-R -m u:@config.wwwuser@:r-X");
-            $project["permissions"]["/php5-fpm"] = $permissionDefinition;
+            $project["permissions"]["/php-fpm"] = $permissionDefinition;
         }
         $this->fileSystemProvider->createDirectory($project, 'tmp');
         {
@@ -65,8 +65,8 @@ class PHPSkeleton extends AbstractSkeleton
         $this->fileSystemProvider->renderDistConfig($this->fileSystemProvider->getConfigTemplateDir("php"),$this->fileSystemProvider->getConfigTemplateDir("php", true),$this->fileSystemProvider->getProjectConfigDirectory($project["name"]) . "/apache.d/");
 
         $this->fileSystemProvider->render(
-            "/php/fcron.d/01php5.twig",
-            $this->fileSystemProvider->getProjectConfigDirectory($project["name"]) . "/fcron.d/01php5",
+            "/php/fcron.d/01php.twig",
+            $this->fileSystemProvider->getProjectConfigDirectory($project["name"]) . "/fcron.d/01php",
             array(
                 "projectdir" => $this->fileSystemProvider->getProjectDirectory($project["name"])
             )
@@ -79,6 +79,7 @@ class PHPSkeleton extends AbstractSkeleton
     public function preMaintenance()
     {
         $this->processProvider->executeSudoCommand("rm -Rf /etc/php5/fpm/pool.d/*");
+        $this->processProvider->executeSudoCommand("rm -Rf /etc/php/7.0/fpm/pool.d/*");
     }
 
     /**
@@ -97,8 +98,20 @@ class PHPSkeleton extends AbstractSkeleton
     {
         $this->processProvider->executeSudoCommand("mkdir -p /etc/php5/fpm/pool.d/");
         $this->fileSystemProvider->render(
-            "/php/php5-fpm.conf.twig",
+            "/php/php-fpm.conf.twig",
             "/etc/php5/fpm/pool.d/" . $project["name"] . ".conf",
+            array(
+                "projectdir" => $this->fileSystemProvider->getProjectDirectory($project["name"]),
+                "projectname" => $project["name"],
+                "projectuser" => $project["name"],
+                "projectgroup" => $project["name"],
+                "develmode" => $this->app["config"]["develmode"]
+            )
+        );
+        $this->processProvider->executeSudoCommand("mkdir -p /etc/php/7.0/fpm/pool.d/");
+        $this->fileSystemProvider->render(
+            "/php/php-fpm.conf.twig",
+            "/etc/php/7.0/fpm/pool.d/" . $project["name"] . ".conf",
             array(
                 "projectdir" => $this->fileSystemProvider->getProjectDirectory($project["name"]),
                 "projectname" => $project["name"],
@@ -135,6 +148,7 @@ class PHPSkeleton extends AbstractSkeleton
     public function preRemove(\ArrayObject $project)
     {
         $this->processProvider->executeSudoCommand("rm -f /etc/php5/fpm/pool.d/".$project["name"].".conf");
+        $this->processProvider->executeSudoCommand("rm -f /etc/php/7.0/fpm/pool.d/".$project["name"].".conf");
     }
 
     /**
